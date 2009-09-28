8c16a605e0e07e00a8f9c4136b2d22037bf1e6b8
Nic Benders <nic.benders@gmail.com>
2009-09-27 18:51:35 -0700

    Works if you put xercesImpl-2.9.1.jar in glassfish lib

--- saml-sinatra.rb
+++ saml-sinatra.rb
@@ -5,13 +5,13 @@ require 'sinatra'
 Dir.glob(File.join(File.dirname(__FILE__), 'java/asf/lib/*.jar')).each {|f| require f }
 
 # require saml manager jar
-Dir.glob(File.join(File.dirname(__FILE__), 'java/asf/dist/*.jar')).each {|f| require f }
+require 'java/asf/dist/saml_factory.jar'
 
 set :port, 7700
 
 PARTNER_ID = 'p0000280'
 KEYSTORE_PASSWORD = 'alltel123'
-CERT_FILE = "config/certs/saml/production/#{PARTNER_ID}.p12"
+CERT_FILE = "config/certs/#{PARTNER_ID}.p12"
 
 get '/saml/cert' do
 	s_mgr = com.alltel.saml.SamlManager.new
@@ -20,4 +20,8 @@ get '/saml/cert' do
 	s_mgr.setKeyEntryPassword(KEYSTORE_PASSWORD)
 	
 	s_mgr.getSamlAssertionXml
+end
+
+get '/saml/ping' do
+  'PONG'
 end
