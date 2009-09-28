53a0a2f0e6e031ae5e3c8c013d9dac58057faeb9
Nic Benders <nic.benders@gmail.com>
2009-09-27 21:08:07 -0700

    Updated Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,14 @@
 set :application, "saml-sinatra"
-set :repository,  "svn+ssh://svn.vocel.com/svn/saml-sinatra/trunk"
+
+default_run_options[:pty] = true
+set :repository,  "git@github.com:benders/saml-sinatra.git"
+set :scm, "git"
+set :branch, "master"
+set :user, "alltelportal"
+set :deploy_via, :remote_cache
+
+ssh_options[:forward_agent] = true
+ssh_options[:user] = "alltelportal"
 
 set :stages, %w(alltel)
 require 'capistrano/ext/multistage'
@@ -8,22 +17,25 @@ after "deploy", "deploy:cleanup"
 
 set :rails_env, 'production'
 
+set :context_root, "/"
+set :jruby_location, "/opt/jruby"
+set :gf_port, "7700"
+
 namespace(:deploy) do
   desc <<-DESC
   Start/Stop/Restart the glassfish process
   DESC
 
   task :start, :roles => :app do
+#    cd #{current_path} &&
      run <<-CMD
-       cd #{current_path} &&
-       RAILS_ENV=#{rails_env} CLASSPATH=.:java/asf/lib/xercesImpl-2.9.1.jar jruby -S glassfish
+       CLASSPATH=.:java/asf/lib/xercesImpl-2.9.1.jar #{jruby_location}/bin/jruby -S glassfish --contextroot #{context_root} --port #{gf_port} --environment #{rails_env} -P #{current_path}/tmp/pids/glassfish.pid --daemon --log-level 7 #{release_path}
      CMD
   end
 
   task :stop, :roles => :app do
      run <<-CMD
-       cd #{current_path} &&
-       kill `cat tmp/pids/glassfish-#{rails_env}.pid`
+       kill `cat #{current_path}/tmp/pids/glassfish.pid`
      CMD
   end
 
